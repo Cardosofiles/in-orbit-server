3282e1074a6cc6989f59cfaecc6feaefe516c593
cardosofiles <cardosofiles@outlook.com>
2025-02-17 22:18:31 -0300

    feat: first route

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,11 +1,48 @@
 import fastify from 'fastify'
+import {
+  type ZodTypeProvider,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
+import { z } from 'zod'
 
-const app = fastify()
+import { createGoal } from '@/services/create-goal'
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.post(
+  '/goals',
+  {
+    schema: {
+      body: z.object({
+        title: z.string(),
+        desiredWeeklyFrequency: z.number().int().min(1).max(7),
+      }),
+    },
+  },
+  async request => {
+    const { title, desiredWeeklyFrequency } = request.body
+
+    await createGoal({
+      title,
+      desiredWeeklyFrequency,
+    })
+  }
+)
 
 app
   .listen({
     port: 3333,
   })
   .then(() => {
-    console.log('HTTP server listening')
+    console.log('🚀 HTTP server listening 🔥')
   })
+
+/***
+ * Validation routes types
+ * https://github.com/turkerdev/fastify-type-provider-zod
+ *
+ */
